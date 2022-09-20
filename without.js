6479ae000f730ea023b94224087df13eeafa09d4
Andrei Chirita <adchir95@gmail.com>
2022-09-20 20:59:59 +0000

    Fixing some mistakes to assertArraysEqual.js and updating with new file without.js

--- without.js
+++ without.js
@@ -24,4 +24,22 @@ const eqArrays = function(array1, array2) {
   }
 }
 
-assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+const without = function(source, itemsToRemove) {
+  let newArr = source;
+  for (let i = 0; i < source.length; i++) {
+    for (let y = 0; y < itemsToRemove.length; y++) {
+      if (source[i] === itemsToRemove[y]) {
+        newArr.splice(i, 1);
+      }
+    }
+  }
+
+  return console.log(newArr);
+}
+
+without([1, 2, 3], [1]);
+without(["1", "2", "3"], [1, 2, "3"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
