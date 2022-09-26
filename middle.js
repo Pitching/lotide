c67ea23c0d997af56f21f641f012c959e87e51ee
Andrei Chirita <adchir95@gmail.com>
2022-09-26 20:40:19 +0000

    exporting middle function and creating middleTest js file

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed, the arrays are a perfect match.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed, the arrays are not a perfect match.`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  let matches = 0;
-  let i;
-
-  if (array1.length === array2.length) {
-    for (i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        matches++;
-      }
-    }
-  }
-  if (i === matches) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 const middle = function(midArray) {
   if (midArray.length <= 2) {
     return [];
@@ -36,4 +10,4 @@ const middle = function(midArray) {
   }
 }
 
-assertArraysEqual(middle([1, 2, 3]), middle([1, 2, 3]));
+module.exports = middle;
